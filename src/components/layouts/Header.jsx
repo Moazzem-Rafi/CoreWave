fe4fd1e79d0314092feb1b1d981afc9bc9471ad3
Moazzem-Rafi <mhr8june@gmail.com>
2026-02-23 00:09:39 +0600

    minorUpdate

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -7,33 +7,33 @@ import Button from "../Button"
 const Header = () => {
     return (
         <>
-        <header className="py-[35px] fixed w-full ">
-            <Container>
-                <div className="flex justify-between items-center">
-                    <div className="flex items-center gap-x-[416px]">
-                        {/* logo */}
-                        <div className="logo">
-                            <Images imgSrc={Logo}/>
+            <header className="py-[35px] fixed w-full ">
+                <Container>
+                    <div className="flex justify-between items-center">
+                        <div className="flex items-center gap-x-[416px]">
+                            {/* logo */}
+                            <div className="logo cursor-pointer">
+                                <Images imgSrc={Logo} />
+                            </div>
+                            {/* navbar */}
+                            <nav>
+                                <ul className="flex gap-x-5 cursor-pointer">
+                                    <li className="hover:text-[#06C279] duration-300">Home</li>
+                                    <li className="hover:text-[#06C279] duration-300">About</li>
+                                    <li className="hover:text-[#06C279] duration-300">Portfolio</li>
+                                    <li className="hover:text-[#06C279] duration-300">Service</li>
+                                    <li className="hover:text-[#06C279] duration-300">Blog</li>
+                                    <li className="hover:text-[#06C279] duration-300">Testimonial</li>
+                                </ul>
+                            </nav>
                         </div>
-                        {/* navbar */}
-                        <nav>
-                            <ul className="flex gap-x-5">
-                                <li>Home</li>
-                                <li>About</li>
-                                <li>Portfolio</li>
-                                <li>Service</li>
-                                <li>Blog</li>
-                                <li>Testimonial</li>
-                            </ul>
-                        </nav>
+                        {/* btn */}
+                        <div className="btn">
+                            <Button btnText={'Register'} className={'px-[25px] py-[14.5px]'} />
+                        </div>
                     </div>
-                    {/* btn */}
-                    <div className="btn">
-                        <Button btnText={'Register'} className={'px-[25px] py-[14.5px]'}/>
-                    </div>
-                </div>
-            </Container>
-        </header>
+                </Container>
+            </header>
         </>
     )
 }
